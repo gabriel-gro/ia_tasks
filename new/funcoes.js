5241f5a2060c26819f9bed6565c2466de2a64abf
limaeduardo <eduardo.fernandodelima@gmail.com>
2018-09-12 09:34:54 -0300

    Retira variáveis globais

--- new/funcoes.js
+++ new/funcoes.js
@@ -1,9 +1,11 @@
 quartos = [{estaLimpo: true, tempo: 0}, {estaLimpo: true, tempo: 0}, {estaLimpo: true, tempo: 0}, {estaLimpo: true, tempo: 0}] // True == Limpo; False == Sujo
 modoAutomatico = false
-quartoAtual = 0
-quartoAnterior = 0
 
 window.onload = function() {
+
+    quartoAtual = 0
+    quartoAnterior = 0
+
     setInterval(function(){
         if(modoAutomatico){
             idQuarto = parseInt((Math.random() * 42 * 10) % 5)
@@ -21,35 +23,42 @@ window.onload = function() {
     },1000)
 
     setInterval(function(){
-        escolheQuarto().then((resultado) => {
+        escolheQuarto(quartoAtual, quartoAnterior).then((resultado) => {
             proximoQuarto = resultado
-            console.log(quartoAnterior, quartoAtual, proximoQuarto)
-            vaiParaOQuatro(proximoQuarto)
-            if (!quartos[quartoAtual].estaLimpo){
-                limpaQuarto(quartoAtual)
-            }
+            vaiParaOQuatro(quartoAtual, proximoQuarto).then((quartoAntAtual) => {
+                console.log(quartoAntAtual)
+                quartoAnterior = quartoAntAtual[0]
+                quartoAtual = quartoAntAtual[1]
+                if (!quartos[quartoAtual].estaLimpo){
+                    limpaQuarto(quartoAtual)
+                }
+            })
         })
     },3000)
 }
 
-function vaiParaOQuatro(idQuarto){
-    quartoAnterior = quartoAtual
-    quartoAtual = idQuarto
-    robo = document.getElementById("robo")
-    switch(idQuarto){
-        case 0:
-            $('#robo').animate({top: '3px',left: '-20px'}, 2000);
-            break;
-        case 1:
-            $('#robo').animate({top: '3px', left: '270px'}, 2000);
-            break;
-        case 2:
-            $('#robo').animate({top: '254px',left: '270px'}, 2000);
-            break;
-        case 3:
-            $('#robo').animate({top: '254px',left: '-20px'}, 2000);
-            break;
-    }
+function vaiParaOQuatro(quartoAtual, proximoQuarto){
+    return new Promise((resolve,reject) => {
+        quartoAnterior = quartoAtual
+        quartoAtual = proximoQuarto
+        robo = document.getElementById("robo")
+        switch(proximoQuarto){
+            case 0:
+                $('#robo').animate({top: '3px',left: '-20px'}, 2000);
+                break;
+            case 1:
+                $('#robo').animate({top: '3px', left: '270px'}, 2000);
+                break;
+            case 2:
+                $('#robo').animate({top: '254px',left: '270px'}, 2000);
+                break;
+            case 3:
+                $('#robo').animate({top: '254px',left: '-20px'}, 2000);
+                break;
+        }
+
+        resolve([quartoAnterior, quartoAtual])
+    })
 }
 
 function toggleModoAutomatico(){
@@ -70,9 +79,9 @@ function sujaQuarto(id){
 }
 
 function limpaQuarto(id){
-    quarto = document.getElementById("quarto"+id)
     document.getElementById("legendaQuarto"+id).innerHTML = "Limpando..."
     setTimeout(function(){
+        quarto = document.getElementById("quarto"+id)
         quarto.style.backgroundColor = "white"
         document.getElementById("legendaQuarto"+id).innerHTML = "Limpo"
         quartos[id].estaLimpo = true
@@ -81,11 +90,10 @@ function limpaQuarto(id){
 
 }
 
-function escolheQuarto(){
+function escolheQuarto(quartoAtual, quartoAnterior){
     return new Promise((resolve,reject) => {
         idMaiorTempo = 0
         maiorTempo = 0
-        console.log(quartos)
         quartos.forEach((quarto, index) => {
         if (quarto.tempo > maiorTempo && index !== quartoAtual && index !== quartoAnterior ){
             idMaiorTempo = index
